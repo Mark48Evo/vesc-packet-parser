00490ecc26bbf2893810be7d70dcb90c53a2fe08
Aurimas Niekis <aurimas@niekis.lt>
2018-06-26 20:39:29 +0100

    1.0.1

--- src/index.js
+++ src/index.js
@@ -11,22 +11,30 @@ export default class VescPacketParser extends Transform {
     super({ ...options, objectMode: true });
   }
 
+  pushResult(type, payload) {
+    return super.push({
+      type,
+      payload,
+    });
+  }
+
   _transform(chunk, encoding, callback) {
     const buffer = new VescBuffer(chunk.payload);
+    const packetType = Object.keys(PacketTypes)[chunk.type];
 
-    debug(`Received PacketType: "${Object.keys(PacketTypes)[chunk.type]}"`);
+    debug(`Received PacketType: "${packetType}"`);
 
     switch (chunk.type) {
       case PacketTypes.COMM_FW_VERSION:
-        getFWVersion(buffer).then(result => this.push(result));
+        getFWVersion(buffer).then(result => this.pushResult(packetType, result));
         break;
 
       case PacketTypes.COMM_GET_VALUES:
-        getValues(buffer).then(result => this.push(result));
+        getValues(buffer).then(result => this.pushResult(packetType, result));
         break;
 
       case PacketTypes.COMM_GET_DECODED_PPM:
-        getDecodedPPM(buffer).then(result => this.push(result));
+        getDecodedPPM(buffer).then(result => this.pushResult(packetType, result));
         break;
 
       default:
